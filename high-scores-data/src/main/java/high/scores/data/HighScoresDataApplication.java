bfd15ea28fc84945ac21a32fc70e1c5d8e6a337c
patm11 <patrick.t.may@gmail.com>
2022-05-31 20:37:32 -0500

    branch1 - Initial commit

---
+++ high-scores-data/src/main/java/high/scores/data/HighScoresDataApplication.java
@@ -0,0 +1,16 @@
+package high.scores.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighScoresDataApplication {
+    /**
+     * Starts the application
+     * 
+     * @param args app arguments
+     */    
+    public static void main(String[] args) {
+        SpringApplication.run(HighScoresDataApplication.class, args);
+    }
+}
